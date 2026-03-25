9a8179ca11dcba8270c1e7847eb9e15f84f38583
Marvin-desmond <marvindesmond@gmail.com>
2026-03-25 03:01:14 -0400

    pass cache instance during prefill and decode

--- src/tiny_sota/inference/utils.py
+++ src/tiny_sota/inference/utils.py
@@ -4,28 +4,32 @@ import re, subprocess, sys
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", "-q", package])
 
+def sample_next_token(logits, top_k=None, temperature=1.0):
+    if top_k is not None:
+        top_logits, _ = torch.topk(logits, top_k)
+        min_val = top_logits[:, -1]
+        logits = torch.where(logits < min_val, torch.tensor(float('-inf')).to(logits.device), logits)
+    if temperature > 0.0:
+        logits = logits / temperature
+        probs = torch.softmax(logits, dim=-1)
+        next_token_id = torch.multinomial(probs, num_samples=1)
+    else:
+        next_token_id = torch.argmax(logits, dim=-1, keepdim=True)
+    return next_token_id
+
 # Llama and Qwen
+@torch.no_grad()
 def generate_text_stream(
-        model, token_ids, max_new_tokens, 
+        model, token_ids, max_new_tokens, cache,
         temperature=0.0, top_k=None, eos_token_id=None):
+    logits = model(token_ids, cache)[:, -1]
     for _ in range(max_new_tokens):
-        with torch.no_grad():
-            logits = model(token_ids)[:, -1]
-        if top_k is not None:
-            top_logits, _ = torch.topk(logits, top_k)
-            min_val = top_logits[:, -1]
-            logits = torch.where(logits < min_val, torch.tensor(float('-inf')).to(logits.device), logits)
-        if temperature > 0.0:
-            logits = logits / temperature
-            probs = torch.softmax(logits, dim=-1)
-            next_token_id = torch.multinomial(probs, num_samples=1)
-        else:
-            next_token_id = torch.argmax(logits, dim=-1, keepdim=True)
+        next_token_id = sample_next_token(logits, top_k, temperature)
         if (eos_token_id is not None
                    and torch.all(next_token_id == eos_token_id)):
             break
         yield next_token_id
-        token_ids = torch.cat((token_ids, next_token_id), dim=1)
+        logits = model(next_token_id, cache)[:, -1]
 
 # Kokoro 
 def tokens_to_text(tokens):
